5f9b76465bdeff4fe868cbf0406b0e8fec3e60e3
voldka <tailx0913@gmail.com>
2023-09-08 01:32:41 +0700

    mongodb cloud

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { config } from "dotenv";
 import cookieParser from 'cookie-parser';
-import dbConnect from "./dbConnect.js";
 import authRoutes from "./routes/auth.js";
 import testRoutes from './routes/test.js';
+import mongoose from "mongoose";
 import refreshTokenRoutes from "./routes/refreshToken.js";
+import connectDB from'./connectMongo.js'
 config();
 
 const app = express()
@@ -16,7 +17,8 @@ app.use("/auth", authRoutes);
 app.use("/auth", refreshTokenRoutes);
 app.use("",testRoutes)
 
-dbConnect();    
+
+connectDB()
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
